8560baca5d0a68296fc999a2daea88600a0b3da3
jeanettepoh <jeanettepoh19@gmail.com>
2024-03-21 23:12:58 +0800

    logging setup

--- src/logger.py
+++ src/logger.py
@@ -7,7 +7,7 @@ logs_dir = os.path.join(os.getcwd(), "logs")
 os.makedirs(logs_dir, exist_ok=True)
 
 # Define log file name with timestamp
-log_filename =f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
+log_filename = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
 log_file = os.path.join(logs_dir, log_filename)
 
 # Configure the logger
@@ -25,7 +25,7 @@ console_handler = logging.StreamHandler()
 console_handler.setFormatter(logging_format)
 logger.addHandler(console_handler)
 
-# Example usage
+# Check if above code works
 if __name__ == "__main__":
     logger.info('This is a log message.')
 
